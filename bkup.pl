36b8926952b8621e4e9ff8e53c03fc0dcd0c6025
Greg Rogalski <greg@grogo.org>
2013-01-15 17:18:56 -0600

    Added symlink to latest version

--- bkup.pl
+++ bkup.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 # VARIABLES 
 
-$CODELOC = "~/webapps/schedule";
+$CODELOC = "/home/mrgschedule/webapps";
+$REPOSLOC = "/home/mrgschedule/repos";
 
 $TMPLOC="/home/mrgschedule/tmp/backup"; # temp staging dir root
 `rm -rf $TMPLOC`; # clean out old garbage
@@ -21,13 +22,10 @@ use POSIX;
 use constant DATETIME => strftime("%a", localtime);
 
 # Create tar file name based on day of week
-#@DAYNAME = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
-#@timelist = localtime(time()); #list of sec,min,hr,day,mo,year,dow,doy
-#$dayofweek = $timelist[6];
-#$DATESTAMP=$DAYNAME[$dayofweek] ;
 
 $DBFILENAME="DB_dump.sql";
 $TARFILE = "schedule." . DATETIME . ".tar";
+$FINAL_NAME = "schedule.latest.tar.gz";
 
 # First make a dump of the databases                                            
 print "Exporting databases\n";
@@ -36,6 +34,10 @@ print "Exporting databases\n";
 # Create TAR file with code and DB dump
 print "Creating TAR file " . $TARFILE . "\n";
 `cd "$TMPLOC"; /bin/tar cvf "$TARFILE" "$DBFILENAME"`;
+# append code
+#print "Appending code and repositories\n";
+#`cd "$TMPLOC"; /bin/tar rvf "$TARFILE" "$CODELOC"`;
+#`cd "$TMPLOC"; /bin/tar rvf "$TARFILE" "$REPOSLOC"`;
 
 # Compress the file                                                             
 print "Compressing TAR file\n";
@@ -44,6 +46,9 @@ print "Compressing TAR file\n";
 # Move gzip'd file to archive loc
 print "Moving archive to " . $BKUP_DEST . "\n";
 `cd "$TMPLOC"; /bin/mv -f "$TARFILE.gz" "$BKUP_DEST"`;
+
+print "Linking archive to " . $FINAL_NAME . "\n";
+`cd "$BKUP_DEST"; /bin/ln -s -f "$TARFILE.gz" "$FINAL_NAME"`;
 
 print "Removing temp files";
 `rm -rf $TMPLOC`; # clean out old garbage
